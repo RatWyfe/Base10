069ceb833c73a69d1b048b6a04e6fb7f70f34a6f
Ratsy <168272212+RatWyfe@users.noreply.github.com>
2024-11-04 17:11:05 +0200

    Base215.cpp
    Rewrote the program, seems the conversion still needs some tweaks

--- test.cpp
+++ test.cpp
@@ -1,51 +1,109 @@
 #include <iostream>
-#include <fstream> 
-#include <vector> 
-#include <string>
+#include <fstream>
 #include <cmath>
+#include <string>
 using namespace std;
 
 int main() {
-	int counter = 0;
-	int asking = 0;
-	int ask = 0;
-	int constant = 0;
-	string buildstr;
-	cin >> ask;
+	// Initiated message
+	cout << "This is base 215=10";
+	cout << "\n";
+	cout << "Decimal or string?(D/S):";
+	cout << "\n";
+
+	int collect = 0;
+
+	bool ToF = false;
+	bool ToS = false;
+	bool end = false;
+
+	string BuildString;
+	string input;
+	string FileName;
 	string line;
-	cout << "type Base64 or SemiUTF-8\n";
-	string filename;
-	string borus;
-	bool boru= false;
-	cin >> borus;
-	if (borus == "Base64") {
-		boru = true;
-	}
-	if (borus == "SemiUTF-8") {
-		boru = true;
-	}
-	if (boru) {
-		ifstream fil(borus);
-		while (getline(fil, line)) {
-			constant++;
+
+	cin >> input;
+
+	// This is for Decimal conversion
+	if (input == "D") {
+		string ToO;
+		cout << "Would you like to convert to base215/base10(215/10):";
+		cout << "\n";
+		cin >> ToO;
+		ToS = false;
+		if (ToO == "10") {
+			ToF = false;
 		}
-		fil.close();
-		while (not (ask == 0)) {
-			counter = 0;
-			asking = ask - (floor(ask / constant)) * constant;
-			ask = floor(ask / constant);
-			ifstream fil(borus);
-			while (getline(fil, line)) {
-				if (asking == counter) {
-					cout << line;
-					buildstr = line + buildstr;
+		if (ToO == "215") {
+			ToF = true;
+		}
+	}
+	else {
+		if (input == "S") {
+			// This is for String conversion
+			string ToO;
+			cout << "Would you like to convert to base215/base10(215/10):";
+			cout << "\n";
+			cin >> ToO;
+			ToS = true;
+			if (ToO == "10") {
+				ToF = false;
+			}
+			if (ToO == "215") {
+				ToF = true;
+			}
+		}
+		else {
+			return 1;
+		}
+	}
+
+	// Here we will convert the values, but string values cannot be interpreted as is for now the best way to do it will be seperating characters with " "
+	cout << "Enter your input:";
+	cout << "\n";
+	if (ToS) {
+		// Collect our string value
+		string collects;
+		cin >> collects;
+		collect = stoi(collects);
+	}
+	else {
+		if (not ToS) {
+			// Collect our integer value
+			cin &gt;&gt; collect;
+		}
+	}
+
+	// Crap, now we need to conver it, first we will have a constant file
+	int countlines = 0;
+	ifstream bst("SemiUTF-8");
+	While (getline(bst, line)) {
+		countlines++;
+	}
+
+	bst.close();
+
+	int finalV = collect;
+
+	if (bst) {
+		While (End)
+		{
+			ifstream bst("SemiUTF-8");
+			int converted = 0;
+			int counter = 0;
+			converted = finalV - (floor(finalV / countlines) * countlines);
+			finalV = floor(finalV / countlines);
+			While (getline(bst, line)) {
+				if (converted == counter) {
+					BuildString = line + BuildString;
 				}
 				counter++;
 			}
-			fil.close();
+			bst.close();
 		}
-		cout << "\n";
-		cout << buildstr;
 	}
-	return 0; 
+	cout &lt;&lt; BuildString;
+	return 3;
+	cout << "Are you satisfied?";
+	cin &gt;&gt; BuildString;
 }
